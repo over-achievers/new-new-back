00c161561bb41ad9e4f08303b7a009b77304d59b
Juan Grajales <grajales.juanfe11@gmail.com>
2019-12-22 15:12:46 -0500

    connect to atlas

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router  = express.Router();
-const User    = require('../models/User');
+const User    = require('../models/user-model');
 
 
 
 // post user info
 router.post('/user', (req, res, next)=>{
-  console.log(req)
+  console.log("The body req is: ", req.body)
+  User.create({
+    username: req.body.username,
+  })
+  .then((response)=>{
+    res.json({response})
+  })
+  .catch((err)=>{
+    next(err)
+  })
 
 })
 
